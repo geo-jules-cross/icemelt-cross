298226807bd86ddd948a6032b07ba42a79877d8c
Julian Cross <jucross@psu.ds.pdx.edu>
2019-07-10 10:00:48 -0700

    Running the base MODIS run.

--- run_all_cluster.sh
+++ run_all_cluster.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=icemelt
 #SBATCH --partition medium
 #SBATCH --ntasks=4
-#SBATCH --output=logs/base-MODIS-07-%A.log
+#SBATCH --output=logs/base-MODIS-%A.log
 #
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
