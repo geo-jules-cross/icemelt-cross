683eeb1b95a00dd5f429cb875e9829973d54b7f5
Julian <julianmcross@gmail.com>
2020-01-07 12:17:44 -0700

    Run setup for MODIS M2 run, no adj

--- icemelt_cross_v05.f95
+++ icemelt_cross_v05.f95
@@ -584,12 +584,12 @@
 ! Note the wordlength (4) is dependent on compiler settings!
         if (iscliff.eq.1) then
         ! mm_met_file='./input/micromet_mjh_cliff/' //   c_i // c_j // '.bin'
-        ! mm_met_file='./input/micromet_jmc_cliff/' //   c_i // c_j // '.bin'
-        mm_met_file='./input/micromet_2deg_cliff/' //   c_i // c_j // '.bin'
+        mm_met_file='./input/micromet_jmc_cliff/' //   c_i // c_j // '.bin'
+        ! mm_met_file='./input/micromet_2deg_cliff/' //   c_i // c_j // '.bin'
         else
         ! mm_met_file='./input/micromet_mjh/' //   c_i // c_j // '.bin'
-        ! mm_met_file='./input/micromet_jmc/' //   c_i // c_j // '.bin'
-        mm_met_file='./input/micromet_2deg/' //   c_i // c_j // '.bin'
+        mm_met_file='./input/micromet_jmc/' //   c_i // c_j // '.bin'
+        ! mm_met_file='./input/micromet_2deg/' //   c_i // c_j // '.bin'
         endif
 
         open (31,file=mm_met_file,access='direct',form='unformatted', &
@@ -624,33 +624,33 @@
                 ! end SELECT
 
                 ! Using station average albedo as per MJH 2016
-                albedo_file = './input/9515_alb.AVG'
+                ! albedo_file = './input/9515_alb.AVG'
                 
                 ! Using new MODIS where available, Update to Thesis Cross 2019
-                ! SELECT CASE (runcell(iii))
-                !     case (10,11,15,16,19,25)                      ! Taylor group
-                !         albedo_file = './input/MODIS_alb_new.TAR'
-                !         print *,'ALBEDO set for TAR'
-                !     case (24,22,23,36,37,38,39)                   ! Hughes/ WKH group
-                !         albedo_file = './input/MODIS_alb_new.HUS'
-                !     case (29)                                     ! Rhone Glacier
-                !         albedo_file = './input/MODIS_alb_new.RHO'
-                !     case (26)                                     ! Matterhorn Glacier
-                !         albedo_file = './input/MODIS_alb_new.MTN'
-                !     case (21)                                     ! LaCroix Glacier
-                !         albedo_file = './input/MODIS_alb_new.LCX'
-                !     case (31,32,33,34)                            ! Suess Glacier
-                !         albedo_file = './input/MODIS_alb_new.SUS'
-                !     case (41,42,43,44,45,61)                      ! Canada Glacier
-                !         albedo_file = './input/MODIS_alb_new.CAA'
-                !         print *,'ALBEDO set for CAA'
-                !     case (71,72,73,74)                            ! Commonwealth Glacier
-                !         albedo_file = './input/MODIS_alb_new.COH'
-                !         print *,'ALBEDO set for COH'
-                !     case (50,62,63,64,65,66,81,82)                ! Howard/ EKH group
-                !         albedo_file = './input/MODIS_alb_new.HOD'
-                !         print *,'ALBEDO set for HOD'
-                ! end SELECT
+                SELECT CASE (runcell(iii))
+                    case (10,11,15,16,19,25)                      ! Taylor group
+                        albedo_file = './input/MODIS_alb_new.TAR'
+                        print *,'ALBEDO set for TAR'
+                    case (24,22,23,36,37,38,39)                   ! Hughes/ WKH group
+                        albedo_file = './input/MODIS_alb_new.HUS'
+                    case (29)                                     ! Rhone Glacier
+                        albedo_file = './input/MODIS_alb_new.RHO'
+                    case (26)                                     ! Matterhorn Glacier
+                        albedo_file = './input/MODIS_alb_new.MTN'
+                    case (21)                                     ! LaCroix Glacier
+                        albedo_file = './input/MODIS_alb_new.LCX'
+                    case (31,32,33,34)                            ! Suess Glacier
+                        albedo_file = './input/MODIS_alb_new.SUS'
+                    case (41,42,43,44,45,61)                      ! Canada Glacier
+                        albedo_file = './input/MODIS_alb_new.CAA'
+                        print *,'ALBEDO set for CAA'
+                    case (71,72,73,74)                            ! Commonwealth Glacier
+                        albedo_file = './input/MODIS_alb_new.COH'
+                        print *,'ALBEDO set for COH'
+                    case (50,62,63,64,65,66,81,82)                ! Howard/ EKH group
+                        albedo_file = './input/MODIS_alb_new.HOD'
+                        print *,'ALBEDO set for HOD'
+                end SELECT
 
                 ! Using combo new MODIS and station albedo, Article Cross & Fountain 2019
                 ! SELECT CASE (runcell(iii))
